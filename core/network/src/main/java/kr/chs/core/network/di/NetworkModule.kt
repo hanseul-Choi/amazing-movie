0142a7d8fd9b5e7ba621cc85ddac7a67dddd9af2
hanseuk-Choi <hschoi5542@gmail.com>
2023-07-24 23:09:42 +0900

    [feat] : TMDB_API Header Interceptor 추가

--- core/network/src/main/java/kr/chs/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/kr/chs/core/network/di/NetworkModule.kt
@@ -6,18 +6,40 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kr.chs.core.network.BuildConfig
 import okhttp3.Call
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class TMDB
+
+    // API_TOKEN Interceptor
+    @Provides
+    @TMDB
+    fun provideTMDBTokenInterceptor() : Interceptor = Interceptor { chain ->
+        with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_KEY}")
+                .build()
+            proceed(newRequest)
+        }
+    }
+
     // default : No Log
     @Provides
     @Singleton
-    fun provideHttpCallFactory() : Call.Factory = OkHttpClient.Builder()
+    @TMDB
+    fun provideHttpCallFactory(
+        tmdbInterceptor: Interceptor
+    ) : Call.Factory = OkHttpClient.Builder()
+        .addInterceptor(tmdbInterceptor)
         .addInterceptor(
             HttpLoggingInterceptor().apply {
                 if(BuildConfig.DEBUG) {
