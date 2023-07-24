444c17df003581b043f5f1f1cf0bd0a078092409
hanseulchoi <lotte.hanseul@gmail.com>
2023-07-24 13:44:26 +0900

    [feat] : app module - hilt Activity 적용

--- app/src/main/java/kr/chs/amazingmovie/AmazingMovieActivity.kt
+++ app/src/main/java/kr/chs/amazingmovie/AmazingMovieActivity.kt
@@ -3,9 +3,11 @@ package kr.chs.amazingmovie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import kr.chs.amazingmovie.ui.AmazingMovieApp
 import kr.chs.core.designsystem.theme.AmazingMovieTheme
 
+@AndroidEntryPoint
 class AmazingMovieActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
