fb7f4fee130e31f2bf97315d16d3262825115938
hanseulchoi <lotte.hanseul@gmail.com>
2023-07-27 08:49:31 +0900

    [fix] : NetworkMovie default 값 생성

--- core/network/src/main/java/kr/chs/core/network/model/NetworkMovie.kt
+++ core/network/src/main/java/kr/chs/core/network/model/NetworkMovie.kt
@@ -6,18 +6,18 @@ import com.google.gson.annotations.SerializedName
 // model의 변수 추가시 network의 확장함수도 변환시켜야함.. -> 한 module에서 변환하려면 model module에서 진행해야하는데 model에 network 의존성이 추가되어 좋지 못할 것 같음
 // 그렇다면? data layer로 책임을 넘기고 model에서 변경 시 그 위의 data module에서도 변경 (=> nowinandroid 방법)
 data class NetworkMovie(
-    @SerializedName("adult") val adult: Boolean,
-    @SerializedName("backdrop_path") val backdropPath: String,
-    @SerializedName("genre_ids") val genreIds: List<Int>,
-    @SerializedName("id") val id: Int,
-    @SerializedName("original_language") val originalLanguage: String,
-    @SerializedName("original_title") val originalTitle: String,
-    @SerializedName("overview") val overview: String,
-    @SerializedName("popularity") val popularity: Double,
-    @SerializedName("poster_path") val posterPath: String,
-    @SerializedName("release_data") val releaseDate: String,
-    @SerializedName("title") val title: String,
-    @SerializedName("video") val video: Boolean,
-    @SerializedName("vote_average") val voteAverage: Double,
-    @SerializedName("vote_count") val voteCount: Long
+    @SerializedName("adult") val adult: Boolean = false,
+    @SerializedName("backdrop_path") val backdropPath: String = " ",
+    @SerializedName("genre_ids") val genreIds: List<Int> = listOf(),
+    @SerializedName("id") val id: Int = 0,
+    @SerializedName("original_language") val originalLanguage: String = "en",
+    @SerializedName("original_title") val originalTitle: String = " ",
+    @SerializedName("overview") val overview: String = " ",
+    @SerializedName("popularity") val popularity: Double = 0.0,
+    @SerializedName("poster_path") val posterPath: String = " ",
+    @SerializedName("release_data") val releaseDate: String = " ",
+    @SerializedName("title") val title: String = " ",
+    @SerializedName("video") val video: Boolean = false,
+    @SerializedName("vote_average") val voteAverage: Double = 0.0,
+    @SerializedName("vote_count") val voteCount: Long = 0
 )
